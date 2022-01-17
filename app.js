676da24d1844d091f7a1eb4e7bb324e0f01b0bc6
svetlanagaydey <lana84@ukr.net>
2022-01-17 21:11:16 +0200

    server part added to front

--- app.js
+++ app.js
@@ -1,13 +1,12 @@
-require("dotenv").config()
 const express = require("express")
-const cors = require("cors")
-const apiRouter = require("./routes/apiRoutes")
-
 const app = express();
-
-app.use(cors());
 app.use(express.json());
+require("dotenv").config();
 
+const cors = require("cors");
+app.use(cors());
+
+const apiRouter = require("./routes/apiRoutes")
 app.use("/api", apiRouter);
 
 app.listen(process.env.PORT, () => {
